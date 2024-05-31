2a3f3623776fa3f46705345503c95936305d1485
Joao Alberto <joaoalbertosv15@gmail.com>
2024-05-30 23:58:07 -0300

    chore: corrects of formatter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Carregar o script externo
+// Criar e inserir o script do Google Analytics
 const script = document.createElement('script');
 script.src = "https://www.googletagmanager.com/gtag/js?id=G-Y543GYC547";
 script.async = true;
@@ -6,6 +6,10 @@ document.head.appendChild(script);
 
 // Definir as variáveis globais
 window.dataLayer = window.dataLayer || [];
-window.gtag = function(){window.dataLayer.push(arguments);}
+window.gtag = function() { window.dataLayer.push(arguments); };
 window.gtag('js', new Date());
-window.gtag('config', 'G-Y543GYC547');
+
+// Configurar o Google Analytics com cookies de primeira parte
+window.gtag('config', 'G-Y543GYC547', {
+  'cookie_flags': 'SameSite=None;Secure'
+});
